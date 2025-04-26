78e2154128a5e3065d92ad6c45ef9550e9f09d53
ilyalinhnguyen <ilyalinhnguyen@gmail.com>
2025-04-26 13:18:04 +0300

    feat: initialize nginx config and dockerfile for nginx

--- backend/src/logger/logger.module.ts
+++
@@ -1,9 +0,0 @@
-// src/logger/logger.module.ts
-import { Module } from '@nestjs/common';
-import { KafkaLoggerService } from './kafka-logger.service';
-
-@Module({
-  providers: [KafkaLoggerService],
-  exports: [KafkaLoggerService],
-})
-export class LoggerModule {}
--- backend_go/go.mod
+++
@@ -1,5 +0,0 @@
-module BackendGoLdap
-
-go 1.24.2
-
-require github.com/go-chi/chi/v5 v5.2.1 // indirect
