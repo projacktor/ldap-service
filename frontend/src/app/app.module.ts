21605339ee851de558e8d76a319b2a16a54bcd58
Projacktor <1234xl.com@gmail.com>
2025-04-26 12:38:06 +0300

    chore: install shadcn

--- frontend/src/app/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { LoggerTestComponent } from './components/logger-test/logger-test.component';
-
-@NgModule({
-  declarations: [
-    LoggerTestComponent
-  ],
-})
-export class AppModule { } 
