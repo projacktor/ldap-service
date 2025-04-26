11d1544ce9dcafd6b4bbe7447c011a7cfb2673f6
Projacktor <1234xl.com@gmail.com>
2025-04-26 18:40:41 +0300

    BREAKING CHANGE: remove backend dir

--- backend/dist/auth/strategies/keycloak/keycloak-config.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { KeycloakConnectOptions, KeycloakConnectOptionsFactory } from 'nest-keycloak-connect';
-import { KeycloakConfig } from '../../../config/keycloak.config';
-export declare class KeycloakConfigService implements KeycloakConnectOptionsFactory {
-    private readonly keycloakConfig;
-    constructor(keycloakConfig: KeycloakConfig);
-    createKeycloakConnectOptions(): KeycloakConnectOptions;
-}
--- backend/dist/logger/logger.module.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=logger.module.js.map
