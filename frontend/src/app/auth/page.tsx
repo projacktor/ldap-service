2dd593bac48137910390adbea223cc5f79b43b89
Projacktor <1234xl.com@gmail.com>
2025-04-26 13:50:40 +0300

    feat: add auth page

--- frontend/src/app/auth/page.tsx
+++ frontend/src/app/auth/page.tsx
@@ -1,17 +1,74 @@
+'use client'
 import React from 'react'
 import { Input } from '@/components/ui/input'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import { Button } from '@/components/ui/button'
+
+const formSchema = z.object({
+  username: z.string().min(4, {
+    message: 'Username must be at least 4 characters.'
+  }),
+  password: z.string().min(8, {
+    message: 'Password must be at least 8 characters.'
+  })
+})
+
+type FormSchema = z.infer<typeof formSchema>
 
 function Page() {
-  return (
-    <main className="content-center">
-      <form className="flex flex-col content-center space-y-4">
-        <h1 className="text-center text-4xl font-bold text-white">Sign in to your account</h1>
-        <p className="text-center text-base font-normal text-gray-500">
-          Enter your email below to enter to your account
-        </p>
-        <Input type="username" placeholder="Enter your username" />
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormSchema>({
+    resolver: zodResolver(formSchema)
+  })
 
-      </form>
+  const onSubmit: SubmitHandler<FormSchema> = (data) => console.log(data)
+
+  return (
+    <main className="flex h-screen w-screen items-center justify-center">
+      <div className="flex flex-col items-center space-y-8">
+        <article className="space-y-4">
+          <h1 className="text-center text-5xl font-bold text-white">Sign in to your account</h1>
+          <p className="text-center text-base font-normal text-gray-400">
+            Enter your email below to enter to your account
+          </p>
+        </article>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col items-center space-y-5 text-white"
+        >
+          <div className="w-90 space-y-2">
+            {errors.username && (<p className="text-left text-sm text-[#c41010]">{errors.username.message}</p>)}
+            <Input
+              id="username"
+              type="text"
+              {...register('username')}
+              placeholder="Username"
+              className="h-10 text-white"
+            />
+          </div>
+
+          <div className="w-90 space-y-2">
+            {errors.password && (
+              <p className="text-sm text-[#c41010]">{errors.password.message}</p>
+            )}
+            <Input
+              id="password"
+              type="password"
+              {...register('password')}
+              placeholder="Password"
+              className="h-10"
+            />
+          </div>
+          <Button type="submit" variant="secondary" className="h-10 w-90">
+            Sign in
+          </Button>
+        </form>
+      </div>
     </main>
   )
 }
